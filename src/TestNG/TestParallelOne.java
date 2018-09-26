f583e20a0a9fd52592aa12a399655b5e48dec5ec
keyur.makwana <kaysmakwana@gmail.com>
2018-09-26 15:27:43 +0530

    Added testng files

---
+++ src/TestNG/TestParallelOne.java
@@ -0,0 +1,26 @@
+package TestNG;
+
+import org.testng.annotations.Test;
+
+public class TestParallelOne {
+	@Test
+	public void testCaseOne() {
+		//Printing Id of the thread on using which test method got executed
+		System.out.println("Test Case One with Thread Id:- "
+				+ Thread.currentThread().getId());
+	}
+
+	@Test
+	public void testCaseTwo() {
+		////Printing Id of the thread on using which test method got executed
+		System.out.println("Test Case two with Thread Id:- "
+				+ Thread.currentThread().getId());
+	}
+	
+	@Test
+	public void testCaseThird() {
+		////Printing Id of the thread on using which test method got executed
+		System.out.println("Test Case third with Thread Id:- "
+				+ Thread.currentThread().getId());
+	}
+}
